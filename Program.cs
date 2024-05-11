1a18c4ada58dd5f4aed9861e32b1e38a11dc14ce
PabloDeJesus <161788354+PabloDeJesus@users.noreply.github.com>
2024-05-11 17:33:45 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Leer un numero entero y determinar si es un numero terminado en 4
+
+Console.WriteLine("Ingrese un número entero:");
+string input = Console.ReadLine();
+
+ if (int.TryParse(input, out int number))
+  {
+  if (number % 10 == 4)
+  {
+
+   Console.WriteLine("El número termina en 4.");
+
+   }
+
+   else
+   {
+
+   Console.WriteLine("El número no termina en 4.");
+
+   }
+
+   }
+   else
+   {
+   Console.WriteLine("Entrada inválida. Por favor, ingrese un número entero válido.");
+   }
+
+
+   Console.ReadLine(); //fin del proyecto
